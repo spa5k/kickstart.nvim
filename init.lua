62aff48862326b04c95c297ba1706f71ffd48655
Spark <79936503+spa5k@users.noreply.github.com>
2024-04-19 15:44:56 +0530

    Update snippet plugin configurations in init.lua and added copilot

--- init.lua
+++ init.lua
@@ -697,12 +697,12 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
       },
       'saadparwaiz1/cmp_luasnip',
@@ -823,6 +823,25 @@ require('lazy').setup({
   --     vim.cmd.hi 'Comment gui=none'
   --   end,
   -- },
+
+  -- Copilot
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    build = ':Copilot auth',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {
+        filetypes = {
+          ['.'] = true,
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+        },
+      }
+    end,
+  },
 
   -- Auto session
   {
